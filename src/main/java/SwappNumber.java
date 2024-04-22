2d44ea7c1755c669f7e2e36f20bb86b0b4f71065
Amit-SDE <amitsamal1998@gmail.com>
2024-04-22 23:07:14 +0530

    Initial Programming

---
+++ src/main/java/SwappNumber.java
@@ -0,0 +1,11 @@
+public class SwappNumber {
+    public static void main(String[] args) {
+        int a= 10;
+        int b = 20;
+        System.out.println("Before swapping : "+a +" " +b);
+        int c=a;
+         a=b;
+         b=c;
+        System.out.println("after swapping :"+a +" "+b);
+    }
+}
