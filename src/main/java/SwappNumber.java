28ce8282af7234c7be43f1977fc1877d7c27ffc5
Amit-SDE <amitsamal1998@gmail.com>
2024-04-24 04:02:28 +0530

    Initial programming

--- src/main/java/SwappNumber.java
+++ src/main/java/SwappNumber.java
@@ -1,11 +1,15 @@
 public class SwappNumber {
+    public static void swap(int a,int b){
+        int c=a;
+        a=b;
+        b=c;
+        System.out.println("after swapping :"+a +" "+b);
+    }
     public static void main(String[] args) {
         int a= 10;
         int b = 20;
         System.out.println("Before swapping : "+a +" " +b);
-        int c=a;
-         a=b;
-         b=c;
-        System.out.println("after swapping :"+a +" "+b);
+        swap(a,b);
+        swap(30,40);
     }
 }
